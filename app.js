e1c8dceabf143aab2ca19ea613f770f2fc538c54
YUHASUNG <yuha822@naver.com>
2022-09-22 14:33:43 +0900

    study: javascript

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-const title = document.getElementById("title");
+const title = document.querySelectorAll(".hello h1"); 
+// 똑같은 클래스 사용할때 전체 가지고 오고 싶으면 꼭 querySelectorAll -> 배열의 형태로 출력 됨
+// 아니면 querySelector 만 사용시 첫 번째만 가지고 옴
 
-title.innerText = "Got you!";
-
-console.log(title.id);
-console.log(title.className);
+console.log(title);
