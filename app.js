949caba7f1f85ec6d7f9b4eb31d2248a826bb266
YUHASUNG <yuha822@naver.com>
2022-09-22 21:50:54 +0900

    study: events

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // 똑같은 클래스 사용할때 전체 가지고 오고 싶으면 꼭 querySelectorAll -> 배열의 형태로 출력 됨
 // 아니면 querySelector 만 사용시 첫 번째만 가지고 옴
 
-const title = document.querySelector(".hello h1:first-child");
+const title = document.querySelector("div.hello:first-child h1");
 // 조건에 맞는 첫 번째를 요소를 가져옴
 
 /*ㅜconst title = document.querySelector("#hello");
@@ -18,4 +18,14 @@ function handleTitleClick(){
     title.style.color = "blue";
 }
 
+function handleMouseEnter(){
+    tilte.innerText = "mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseEnter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
