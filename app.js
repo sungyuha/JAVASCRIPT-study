6f6c18f88a9a7f1660153b3d80c61a84fa74c9e5
YUHASUNG <yuha822@naver.com>
2022-09-29 23:08:38 +0900

    study: js and toggle event

--- app.js
+++ app.js
@@ -5,14 +5,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
-    const clickedClass = "clicked";
-    if(h1.classList.contains(clickedClass)){ 
-        h1.className = ""; 
-        // = 면  해당 되는 값을 className 값으로 변경
+    const clickedClass = ("clicked");
+     // toggle은 h1의 ClassList에 clicked class가 이미 있는지 확인해서 만약 있다면, toggle이 cliked를 제거해줌
+    // 만약, h1의 classList에 clicked가 존재하지 않는다면 toggle은 clicked를 classList에 추가 해줌
+    if (h1.classList.contains(clickedClass)){
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = "clickedClass";
-        // string을 두 번 사용하면 에레 발생할 확률이 높아짐
+        h1.classList.add(clickedClass);
     }
-}
-
+    h1.classList.toggle(clicked);
+}    
 h1.addEventListener("click", handleTitleClick);
